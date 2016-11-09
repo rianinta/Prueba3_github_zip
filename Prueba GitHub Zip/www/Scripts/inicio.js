a8c43354dd72ddf7cf3a76d441875d5c4428fb9c
LibroZSRianInta <ramos.lucas@inta.gob.ar>
2016-11-09 12:03:48 -0300

    Prueba icono app y carga datos desde afuera
    carga de datos con ajax!

--- Prueba GitHub Zip/www/Scripts/inicio.js
+++ Prueba GitHub Zip/www/Scripts/inicio.js
@@ -2,4 +2,28 @@ $(function(){
 	alert("Hola! Ya cargué jQuery! :)")
 
 	$("#modificado").text("Este sería un texto que cambió")
+
+	$.ajax({
+            type: "POST",
+            url: "http://riancarga.inta.gob.ar/WsEAR/TraeEstadisticas.aspx/TraeCultivosPorProvincia",
+            data: '{pIdProvincia: "22"}',
+            cache: false,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: CargoCultivosEnCombo,
+            failure: function(response) {
+                alert(response.d);
+            }
+        });
 });
+
+function CargoCultivosEnCombo(response){
+    var datos = jQuery.parseJSON(response.d);
+    var MuestroLalala = ''
+
+    $.each(datos, function(index, Valores) {
+        MuestroLalala = MuestroLalala + "(" + Valores.IdCultivo + ") " + Valores.Cultivo + "|"
+    });
+
+    alert(MuestroLalala)
+}
